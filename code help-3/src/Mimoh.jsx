b044f42d98b9a6468760712bebaf2ba3f9b66be2
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-01 14:22:51 +0530

    learning about hooks in react

--- code help-3/src/Mimoh.jsx
+++ code help-3/src/Mimoh.jsx
@@ -7,18 +7,28 @@ function Mimoh(prop) {
   console.log(prop.colledge);
   const [newName, setNewName] = useState(prop.name);
   const [newNumber, setNewNumber] = useState(prop.number);
+  // const [submit, setsubmit] = useState(prop.number);
   // const [newName, setNewName] = useState(prop.name);
   function handleEvent() {
-    const newName="manish"
+    const newName = "manish";
     setNewName(newName);
   }
   function handleEvent2() {
-    setNewNumber(7233091999)
-    
+    setNewNumber(7233091999);
   }
+  // function handleChange(params) {
+  //   console.log();
+  //   setsubmit(params.target.value);
+  // }
 
   return (
     <>
+      {/* <input
+        onChange={handleChange}
+        type="text"
+        placeholder="write something"
+      /> */}
+      {/* <button onClick={submit}>submit</button> */}
       <div>{newName}</div>
       <button onClick={handleEvent}>Change the content </button>
       <div>{newNumber}</div>
