773092bad5be535b5a1f6fb25238db283b06c14b
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-03 00:11:34 +0530

    commit

--- code help -7/src/App.jsx
+++ code help -7/src/App.jsx
@@ -7,9 +7,10 @@ function App() {
   // console.log(reviews)
 
   return (
-    <div>
-      <div>
-        <h1>Our Testimonials</h1>
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center bg-gray-200">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Our Testimonials</h1>
+        <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
 
         <Testimonial reviews={reviews}></Testimonial>
       </div>
--- code help -7/src/Card.jsx
+++ code help -7/src/Card.jsx
@@ -1,20 +1,33 @@
 // import React from 'react'
+import {FaQuoteLeft,FaQuoteRight} from "react-icons/fa"
 
 function Card(props) {
   // console.log(props);
   const reviews = props.reviews;
   // console.log(reviews[0]);
   return (
-    <div>
-      <div>
-        <img src="reviews.image" />
+    <div className="flex flex-col md:relative">
+      <div className="absolute top-[-7rem] z-10 mx-auto ">
+        <img  className="aspect-square rounded-full w-[140px] h-[140px] z-25"
+        
+        src={reviews.image}></img>
+        <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px]"></div>
       </div>
-      <div>
-        <p>{reviews.name}</p>
+      <div className="text-center mt-7">
+        <p className="font-bold text-2xl capitalize">{reviews.name}</p>
       </div>
-      <div>
-        {reviews.job}
+      <div className="text-center mt-7">
+        <p className="text-violet-300">{reviews.job}</p>
       </div>
+      <div className="text-violet-400 mx-auto mt-5">
+        <FaQuoteLeft></FaQuoteLeft>
+      </div>
+      <div className="text-center mt-4 text-slate-500">{reviews.text}</div>
+
+      <div>
+        <FaQuoteRight className="text-violet-400 mx-auto mt-5"></FaQuoteRight>
+      </div>
+      
     </div>
   );
 }
