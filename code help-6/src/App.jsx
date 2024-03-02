0d6cfa4469b62d846f5982a0a1c2816cfbf52871
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-02 14:54:56 +0530

    api call and fetching data

--- code help-6/src/App.jsx
+++ code help-6/src/App.jsx
@@ -1,27 +1,56 @@
 // import { useState } from 'react'
-import Cards from './components/Cards'
-import Filter from './components/Filter'
-import Navbar from './components/Navbar'
+import { useEffect, useState } from "react";
+import Cards from "./components/Cards";
+import Filter from "./components/Filter";
+import Navbar from "./components/Navbar";
 // import './App.css'
-import { apiUrl,filterData } from './Data'
+import { apiUrl, filterData } from "./Data";
+
+import { toast } from "react-toastify";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchData() {
+    setLoading(true)
+   try {
+    const response= await fetch(apiUrl)
+   const responseData=await response.json();
+   console.log(responseData);
+   setCourses(responseData)
+    
+   } catch (error) {
+toast.error( "Error Occured while Fetching Data");
+    
+   }
+setLoading(false)
+
+  }
+  
+ useEffect(()=>{
+  fetchData()
+ },[])
+
+
+
+
 
   return (
     <>
-    <div>
-      <Navbar></Navbar>
-    </div>
-    <div>
-      <Filter filterData={filterData}></Filter>
-    </div>
-    <div>
-      <Cards></Cards>
-    </div>
-
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div>
+        <Filter filterData={filterData}></Filter>
+      </div>
+      <div>
+        {
+          loading?()
+        }
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- code help-6/src/components/Filter.jsx
+++ code help-6/src/components/Filter.jsx
@@ -1,11 +1,21 @@
 // import React from 'react'
 
 function Filter(prop) {
-  let filterData = prop.filterData;
-  // console.log(filterData);
-  return filterData.map((data) => {
-    console.log(data.title);
-  });
-}
+    // console.log(prop);
+    
+    const filterData=prop.filterData
+    // console.log(prop);
+    // console.log(filterData);
+  return (
 
-export default Filter;
+   
+
+    
+    filterData.map((data,index)=>(
+        <button key={index}>{data.title}</button>
+    ))
+  )
+
+    }
+
+export default Filter
