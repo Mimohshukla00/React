214538cf5df19d01b57dd0d29dd5ca4d7f10ce61
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-03 21:25:47 +0530

    commit

--- code help-intermediate-1/src/App.jsx
+++ code help-intermediate-1/src/App.jsx
@@ -1,18 +1,20 @@
+// import "./App.css";
 import { useState } from "react";
-// import './App.css'
 
 function App() {
-  // const [firstname, setfirstname] = useState("")
-  // const [secondname, setsecondname] = useState("")
-  // function changeHandle1(params) {
-  //   // console.log(params.target.value)
-  //   setfirstname(params.target.value);
-  //   // console.log(firstname)
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
 
+  // function changeFirstNameHandler(event) {
+  //   // console.log(event.target);
+  //   console.log(event.target.value);
+  //   setFirstName(event.target.value);
   // }
-  // function changeHandle2(params) {
-  //   console.log(params.target.value)
-  //   setsecondname(params.target.value);
+
+  // function changeLastNameHandler(event) {
+  //   // console.log(event.target);
+  //   console.log(event.target.value);
+  //   setLastName(event.target.value);
   // }
 
   const [formData, setFormData] = useState({
@@ -20,76 +22,134 @@ function App() {
     lastName: "",
     email: "",
     comment: "",
-    isvisible: true,
+    isAgree: true,
+    mode: "",
+    favCar: ""
   });
-  function changeHandle(event) {
-    console.log(formData);
-    const { name, value, Checked, type } = event.target;
-    setFormData((prevformData) => {
-      // console.log(...prevformData);
-      // console.log(event.target.value)
 
+  // console.log(formData);
+
+  function changeHandler(event) {
+    const { name, value, checked, type } = event.target;
+    setFormData((prevFormData) => {
+      // console.log(prevFormData);
       return {
-        ...prevformData,
-        // [event.target.name]: event.target.value,
-        [name]: type === "checkbox" ? Checked : value,
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
       };
     });
   }
 
+  function submitHandler(event)
+  {
+    event.preventDefault();
+    console.log("Form Give DAta")
+    console.log(formData);
+  }
+
   return (
-    <div>
-      <form>
-        <input
-          type="text"
-          placeholder="first name"
-          onChange={changeHandle}
-          name="FirstName"
-          value={formData.FirstName}
-        />
-        <br></br>
-        <br></br>
-        <br></br>
-        <input
-          type="text"
-          placeholder="second name"
-          onChange={changeHandle}
-          name="lastname"
-          value={formData.LastName}
-        />
-        <br></br>
-        <br></br>
-        <br></br>
-        <input
-          type="email"
-          placeholder="enter your email"
-          onChange={changeHandle}
-          name="email"
-          value={formData.Email}
-        />
+    <div className="App">
+      <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Enter First name"
+            // onChange={changeFirstNameHandler}
+            onChange={changeHandler}
+            name="firstName"
+            value={formData.firstName}
+          />
+
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Enter Last name"
+            // onChange={changeLastNameHandler}
+            onChange={changeHandler}
+            name="lastName"
+            value={formData.lastName}
+          />
+
+          <br />
+          <br />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            onChange={changeHandler}
+            name="email"
+            value={formData.email}
+          />
+
+          <br />
+          <br />
+          <textarea
+            name="comment"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Enter your Comments"
+            onChange={changeHandler}
+            value={formData.value}
+          ></textarea>
+
+          <br />
+          <br />
+          <input
+            type="checkbox"
+            name="isAgree"
+            onChange={changeHandler}
+            id="isAgree"
+            checked={formData.isAgree}
+          />
+
+          <label htmlFor="isAgree">Agree Term and Condition</label>
+
+          <br />
+          <br />
+
+          <fieldset> 
+
+          <input
+            type="radio"
+            name="mode"
+            id="Dark-mode"
+            value="Dark-mode"
+            onChange={changeHandler}
+            checked={formData.mode === "Dark-mode"}
+          />
+
+          <label htmlFor="Dark-mode">Dark Mode</label>
+
+          <input
+            type="radio"
+            name="mode"
+            id="Light-mode"
+            value="Light-mode"
+            onChange={changeHandler}
+            checked={formData.mode === "Light-mode"}
+          />
+          <label htmlFor="Light-mode">Light Mode</label>
+        </fieldset>
+
+        <select
+          name="favCar"
+          id="favCar"
+          value={formData.favCar}
+          onChange={changeHandler}
+        >
+          <option value="scarpio">Scarpio</option>
+          <option value="russian">Russian</option>
+          <option value="thar">Thar</option>
+          <option value="bmw">BMW</option>
+          <option value="other" selected>Other</option>
+        </select>
+
+        <label htmlFor="favCar">Your Favrioute Car</label>
+
+        <br /><br />
+
+        <input type="submit" value="Submit" />
       </form>
-
-      <br></br>
-      <br></br>
-      <br></br>
-      <textarea
-        placeholder="enter your comments here "
-        onChange={changeHandle}
-        value={formData.comment}
-        name="comment"
-      ></textarea>
-
-      <br></br>
-      <br></br>
-      <br></br>
-      <input
-        id="isvisible"
-        onClick={changeHandle}
-        type="checkbox"
-        name="isvisible"
-        checked={formData.isvisible}
-      ></input>
-      <label htmlFor="isvisible">Am i visible</label>
     </div>
   );
 }
