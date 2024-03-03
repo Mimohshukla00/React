3ca8bf5c2d1ef42bfd6ca6e12cc367747e4a83d5
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-03 21:17:16 +0530

    commit

--- code help-intermediate-1/src/App.jsx
+++ code help-intermediate-1/src/App.jsx
@@ -19,16 +19,20 @@ function App() {
     firstName: "",
     lastName: "",
     email : "" ,
+    comment:"",
+    isvisible:true,
   });
   function changeHandle(event) {
-    // console.log(formData);
+    console.log(formData);
+    const {name,value,Checked,type}=event.target;
     setFormData((prevformData) => {
       // console.log(...prevformData);
-      console.log(event.target.value)
+      // console.log(event.target.value)
       
       return {
         ...prevformData,
-        [event.target.name]: event.target.value,
+        // [event.target.name]: event.target.value,
+        [name]: type === "checkbox" ?Checked : value
       };
     });
   }
@@ -41,20 +45,42 @@ function App() {
           placeholder="first name"
           onChange={changeHandle}
           name="FirstName"
+          value={formData.FirstName}
         />
+        <br></br>
+        <br></br>
+        <br></br>
         <input
           type="text"
           placeholder="second name"
           onChange={changeHandle}
           name="lastname"
+          value={formData.LastName}
         />
+        <br></br>
+        <br></br>
+        <br></br>
         <input
           type="email"
           placeholder="enter your email"
           onChange={changeHandle}
           name="email"
+          value={formData.Email}
         />
       </form>
+
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <textarea placeholder="enter your comments here " onChange={changeHandle} value={formData.comment} name="comment"></textarea>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <input  id="isvisible"  onClick={changeHandle} type="checkbox" name="isvisible" checked={formData.isvisible}></input>
+      <label htmlFor="isvisible">Am i visible</label>
+      
     </div>
   );
 }
