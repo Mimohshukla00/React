76c92eba19129806597169251a847c391889e770
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-03 20:36:21 +0530

    handle several hooks by using one hook

--- code help-intermediate-1/src/App.jsx
+++ code help-intermediate-1/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [firstname, setfirstname] = useState("")
+  // const [secondname, setsecondname] = useState("")
+  // function changeHandle1(params) {
+  //   // console.log(params.target.value)
+  //   setfirstname(params.target.value);
+  //   // console.log(firstname)
+
+  // }
+  // function changeHandle2(params) {
+  //   console.log(params.target.value)
+  //   setsecondname(params.target.value);
+  // }
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email : "" ,
+  });
+  function changeHandle(event) {
+    setFormData((prevformData) => {
+      return {
+        ...prevformData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="first name"
+          onChange={changeHandle}
+          name="FirstName"
+        />
+        <input
+          type="text"
+          placeholder="second name"
+          onChange={changeHandle}
+          name="lastname"
+        />
+        <input
+          type="email"
+          placeholder="enter your email"
+          onChange={changeHandle}
+          name="email"
+        />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
