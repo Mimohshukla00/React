e918d4e6666eac8a5cc056f23a3143a74e7d629c
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-04 22:48:19 +0530

    commit

--- reactForm/src/App.jsx
+++ reactForm/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [formdata, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    country: ""
+  });
+  // console.log(formdata.firstName)
+  function handleClick(event) {
+    // console.log(event);
+    setFormData((prevdata) => {
+      console.log(formdata.country)
+      // console.log(prevdata);
+      return { ...prevdata, [event.target.name]: event.target.value };
+    });
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form>
+        <label htmlFor="firstName">First Name</label>
+        <br></br>
+        <input
+          id="firstName"
+          type="text"
+          placeholder="Enter FirstName"
+          name="firstName"
+          onChange={handleClick}
+          value={formdata.firstName}
+        ></input>
+        <br></br>
+        <label htmlFor="lastName">List Name</label>
+        <br></br>
+        <input
+          id="lastName"
+          type="text"
+          placeholder="Enter Last Name"
+          name="lastName"
+          onChange={handleClick}
+        ></input>
+        <br></br>
+        <label htmlFor="email">Email</label>
+        <br></br>
+        <input
+          id="email"
+          type="email"
+          placeholder="Enter Email"
+          name="email"
+          onChange={handleClick}
+        ></input>
+        <br></br>
+        <label htmlFor="dropDown">Country</label>
+        <br></br>
+
+        <select
+          id="dropDown"
+          name="country"
+          value={formdata.country}
+          onChange={handleClick}
+        >
+          <option 
+          value="formdata.India">India</option>
+          <option value={formdata.Nepal}>Nepal</option>
+          <option value={formdata.Australia}>Australia</option>
+          <option value={formdata.Japan}>Japan</option>
+        </select>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
