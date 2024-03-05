d8c6c7f6c84cd4ca645082449533d85c9150b9f0
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-06 01:29:13 +0530

    commit

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,12 +1,69 @@
-// import { useState } from 'react'
+// import React from 'react'
+import {Routes,Route,NavLink} from  "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Support from "./Components/Support";
+import Labs from "./Components/Labs";
+import NotFound from "./Components/NotFound";
+import MainHeader from "./Components/MainHeader";
+import "./App.css"
 
-// import './App.css'
+
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <div>mimohshukla</div>
+    <div>
+
+
+      
+    <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/support">Support</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/labs">Labs</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/support" element={<Support />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/labs" element={<Labs />} />
+    <Route path="*" element={<NotFound />} />
+    <Route path="/" element={<MainHeader />}>
+      {/* Default Route 
+      {/* <Route index element={<Home />} />
+      <Route path="/support" element={<Support />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/labs" element={<Labs />} />
+      <Route path="*" element={<NotFound />} /> */}
+    </Route>
+  </Routes>
+    </div>
+
+
   )
 }
 
