a2a4def145e2eea29a98fd4c5d639461df70a0cb
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-08 00:35:28 +0530

    commit

--- Intermediate-Project/src/Components/Navbar.jsx
+++ Intermediate-Project/src/Components/Navbar.jsx
@@ -1,9 +1,29 @@
 // import React from 'react'
+import {Link} from "react-router-dom"
+import logo from  "../assets/Logo.svg";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <Link to="/">
+        <img src={logo} alt="Company Logo" id='navbar-logo'/>
+      </Link>
+      <ul>
+
+        <li>
+          <Link to="/" >Home</Link>
+        </li>
+        <li>
+          <Link to="/" >Contact</Link>
+        </li>
+        <li>
+          <Link to="/" >About</Link>
+        </li>
+        
+      </ul>
+    
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
