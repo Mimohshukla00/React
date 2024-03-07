5f4e5f7f61a2749613ce323be9962f1eaebb1d1e
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-08 00:52:48 +0530

    commit

--- Intermediate-Project/src/Components/Navbar.jsx
+++ Intermediate-Project/src/Components/Navbar.jsx
@@ -1,27 +1,57 @@
 // import React from 'react'
-import {Link} from "react-router-dom"
-import logo from  "../assets/Logo.svg";
+import { Link } from "react-router-dom";
+import logo from "../assets/Logo.svg";
+import { useState } from "react";
 
-function Navbar() {
+function Navbar(props) {
+  // console.log(props.isLoggedIn);
+  const isLoggedIn=props.isLoggedIn
+  const setIsLoggedIn=props.setIsLoggedIn
   return (
     <div>
       <Link to="/">
-        <img src={logo} alt="Company Logo" id='navbar-logo'/>
+        <img src={logo} alt="Company Logo" id="navbar-logo" />
       </Link>
-      <ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+        </ul>
+      </nav>
 
-        <li>
-          <Link to="/" >Home</Link>
-        </li>
-        <li>
-          <Link to="/" >Contact</Link>
-        </li>
-        <li>
-          <Link to="/" >About</Link>
-        </li>
-        
-      </ul>
-    
+      <div>
+
+        {
+          !isLoggedIn &&
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        }
+        {
+          !isLoggedIn &&
+          <Link to="/signup">
+            <button>Signup</button>
+          </Link>
+        }
+        { isLoggedIn &&
+          <Link to="/">
+            <button>Log Out</button>
+          </Link>
+        }
+        {
+          isLoggedIn &&
+          <Link to="/Dashboard">
+            <button>Dashboard</button>
+          </Link>
+        }
+      </div>
     </div>
   );
 }
