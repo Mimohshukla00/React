2740d39c33894803d5146c47939a84bd1fa225e5
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-07 22:36:45 +0530

    commit

--- studyNotionProject/src/App.jsx
+++ studyNotionProject/src/App.jsx
@@ -1,17 +1,9 @@
-// import { useState } from 'react'
-
-// import './App.css'
+import React from 'react'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
-
+    <div>MImoshu</div>
     
-    
-    </>
-  
   )
 }
 
