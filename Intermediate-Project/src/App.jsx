b8d4a9aaffee6c9626d9fc5b8daa71a9e3e7e0c4
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-08 00:08:32 +0530

    commit

--- Intermediate-Project/src/App.jsx
+++ Intermediate-Project/src/App.jsx
@@ -1,6 +1,11 @@
 // import { useState } from 'react'
 import { Route, Router } from "react-router-dom"
 import Navbar from "./Components/Navbar"
+// import Home from "../../02 - React Intermediate/react-router/router-project-studyNotation/src/Components/Home"
+import Home from "./Pages/Home"
+import Login from "./Pages/Login"
+import Signup from "./Pages/Signup"
+import Dashboard from "./Pages/Dashboard"
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -11,10 +16,10 @@ function App() {
       <Navbar></Navbar>
 
       <Router>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+        <Route path="/" element={<Home></Home>} ></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="Dashboard" element={<Dashboard></Dashboard>}></Route>
       </Router>
     </div>
 
