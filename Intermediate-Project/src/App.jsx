8405f6dee5507ab1cf9730211b8fdcd31a895e5b
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-07 23:55:44 +0530

    commit

--- Intermediate-Project/src/App.jsx
+++ Intermediate-Project/src/App.jsx
@@ -1,11 +1,23 @@
 // import { useState } from 'react'
+import { Route, Router } from "react-router-dom"
+import Navbar from "./Components/Navbar"
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hello World!</h1>
+    <div>
+      <Navbar></Navbar>
+
+      <Router>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Router>
+    </div>
+
    
     </>
   )
