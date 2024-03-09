c4987e72afd3cc0ccb50d8a9582e4d6e8d1a73fe
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-09 22:13:25 +0530

    commit

--- randomGifgenerator/src/components/Random.jsx
+++ randomGifgenerator/src/components/Random.jsx
@@ -1,8 +1,21 @@
 // import React from 'react'
 
+import { useState } from "react"
+
 function Random() {
+  const [gif,setgif]=useState("")
+
+  function clickhandler(p) {
+    console.log(p);
+    
+  }
   return (
-    <div>Random</div>
+    <div className="w-1/2 h-[450px] bg-green-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+      <h1 className="text-3xl uppercase underline font-bold">Random Gif</h1>
+      <img src={gif} width="450"/>
+      <button onClick={clickhandler}>Generate</button>
+
+    </div>
   )
 }
 
