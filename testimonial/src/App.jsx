c839020872739a1050da74dc4bae4605cb7907e1
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-12 23:31:55 +0530

    commit

--- testimonial/src/App.jsx
+++ testimonial/src/App.jsx
@@ -1,20 +1,19 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+// import React from "react";
+// import Testimonial from "./Components/Testimonial.jsx";
+import Testimonial from "./components/Testimonial";
+import reviews from "./data";
+// import Card from "./components/Card";
 
-function App() {
-  // const [count, setCount] = useState(0)
-
-
-
-
+const App = () => {
   return (
-    <>
-    <div>mimohshukla</div>
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Our Testimonial</h1>
+        <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+        <Testimonial reviews = {reviews}/>
+      </div>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
