06573c7d656cbc25d30d681b95715e9a3da34c5c
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-16 00:24:24 +0530

    no page

--- vite-project/src/Pages/NoPage.jsx
+++ vite-project/src/Pages/NoPage.jsx
@@ -1,9 +1,15 @@
 // import React from 'react'
 
+import { Outlet } from "react-router-dom";
+
 function NoPage() {
   return (
-    <div>NoPage</div>
-  )
+    <div>
+      <div>
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 }
 
-export default NoPage
+export default NoPage;
