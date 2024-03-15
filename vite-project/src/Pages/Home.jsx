ab75b609f7552d9569275ce3ca10e900bc848424
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-16 00:24:15 +0530

    home

--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,9 +1,15 @@
-// import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return (
+  const navigate=useNavigate()
+  return;
+
+  <div>
     <div>Home</div>
-  )
+    <button onClick={() => navigate("/")}>next Page </button>
+    <button onClick={() => navigate(-1)}>Prvious page Page </button>
+  </div>;
 }
 
-export default Home
+export default Home;
