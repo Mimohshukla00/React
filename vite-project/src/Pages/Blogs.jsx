34ca8cf6c88de86c46bc5eea5f1bed3a14eaae1c
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-16 00:24:00 +0530

    blog

--- vite-project/src/Pages/Blogs.jsx
+++ vite-project/src/Pages/Blogs.jsx
@@ -1,9 +1,16 @@
 // import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 function Blogs() {
+  const navigate = useNavigate();
+
   return (
-    <div>Blogs</div>
-  )
+    <div>
+      <div>Blogs</div>
+      <button onClick={() => navigate("/Contact")}>next Page </button>
+      <button onClick={() => navigate(-1)}>Prvious page Page </button>
+    </div>
+  );
 }
 
-export default Blogs
+export default Blogs;
