0d2c70117f5c1dd8423a3e5a001b7f8e44367934
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-16 00:24:08 +0530

    contact

--- vite-project/src/Pages/Contact.jsx
+++ vite-project/src/Pages/Contact.jsx
@@ -1,9 +1,23 @@
-// import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Contact() {
+  const navigate = useNavigate();
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <div className="">
+        <div>Contact</div>
+        <button
+          onClick={() => {
+            navigate("/Blogs");
+          }}
+        >
+          Go to Blogs
+        </button>
+        <button onClick={() => navigate(-1)}>Go Back</button>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
