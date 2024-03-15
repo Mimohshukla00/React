9ac0b7651f8c7783fd2dcacf4b52d3dd33bf620a
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-15 15:28:13 +0530

    App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Pages/Layout";
+import Home from "./Pages/Home";
+import Blogs from "./Pages/Blogs";
+import Contact from "./Pages/Contact";
+// import NoPage from "./Pages/NoPage";
+import { NavLink } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/support">Support</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/labs">Labs</NavLink>
+          </li>
+          <li>
+            <NavLink to="*">No page Found</NavLink>
+          </li>
+          <li>
+            <NavLink to="contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/support" element={<Layout />} />
+        <Route path="/about" element={<Layout />} />
+        {/* <Route path="/labs" element={<NoPage />} /> */}
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/Blogs" element={<Blogs />}>
+          {/* Default Route 
+      {/* <Route index element={<Home />} />
+      <Route path="/support" element={<Support />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/labs" element={<Labs />} />
+      <Route path="*" element={<NotFound />} /> */}
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
