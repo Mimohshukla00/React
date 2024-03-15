98207c958b6f94d4d6d80844e345c8af33d7d72e
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-15 15:28:06 +0530

    commit new project

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,22 +1,17 @@
 // import React from 'react'
-import {Routes,Route,NavLink} from  "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Support from "./Components/Support";
 import Labs from "./Components/Labs";
 import NotFound from "./Components/NotFound";
 import MainHeader from "./Components/MainHeader";
-import "./App.css"
-
-
+import "./App.css";
 
 function App() {
   return (
     <div>
-
-
-      
-    <nav>
+      <nav>
         <ul>
           <li>
             <NavLink to="/">Home</NavLink>
@@ -33,38 +28,23 @@ function App() {
         </ul>
       </nav>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/support" element={<Support />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/labs" element={<Labs />} />
-    <Route path="*" element={<NotFound />} />
-    <Route path="/" element={<MainHeader />}>
-      {/* Default Route 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/labs" element={<Labs />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<MainHeader />}>
+          {/* Default Route 
       {/* <Route index element={<Home />} />
       <Route path="/support" element={<Support />} />
       <Route path="/about" element={<About />} />
       <Route path="/labs" element={<Labs />} />
       <Route path="*" element={<NotFound />} /> */}
-    </Route>
-  </Routes>
+        </Route>
+      </Routes>
     </div>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
