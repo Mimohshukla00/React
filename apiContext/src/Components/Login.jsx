ac6813c5603917b0fe30c2f6128e003a3aea45d7
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-03-23 01:26:43 +0530

    Login componnets

---
+++ apiContext/src/Components/Login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import UserContext from "../Context/Usercontext";
+import { useState } from "react";
+// import UserContext from "../Context/Usercontext";
+ import { useContext } from "react";
+
+function Login() {
+  const [UserName, setUserName] = useState("");
+  const [password, setpassword] = useState("");
+  function handlechange(params) {
+   setUserName(params.target.value);
+   console.log(params.target.value)
+    
+  }
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" placeholder="Username" value={UserName} onChange={handlechange}></input>
+
+    </div>
+  );
+}
+
+export default Login;
