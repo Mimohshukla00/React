1f470bd1bfb5397812acb69f2fe6615bd1a1e0f3
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-06 22:45:43 +0530

    commit

--- blogwebsite/src/main.jsx
+++ blogwebsite/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import AppContextProvider from "./Context/AppContextProvider.jsx";
 // import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppContextProvider>
     <App />
-  </React.StrictMode>,
-)
+  </AppContextProvider>
+);
