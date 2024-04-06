ccb0c29da72b3ccd15ef89e354a0d42c52425ce7
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-06 22:45:37 +0530

    commit

--- blogwebsite/src/App.jsx
+++ blogwebsite/src/App.jsx
@@ -7,13 +7,11 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <Header></Header>
-        <Blogs></Blogs>
-        <Pagination></Pagination>
-      </div>
-    </>
+    <div>
+      <Header></Header>
+      <Blogs></Blogs>
+      <Pagination></Pagination>
+    </div>
   );
 }
 
