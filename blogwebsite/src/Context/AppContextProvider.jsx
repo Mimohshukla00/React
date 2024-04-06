829b4c8d47813f0dc1ebf93711027e842d1a4ba6
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-06 22:45:50 +0530

    commit

--- blogwebsite/src/Context/AppContextProvider.jsx
+++ blogwebsite/src/Context/AppContextProvider.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from "react";
+import AppContext from "./AppContext";
+function AppContextProvider({ children }) {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
 
-function AppContextProvider({children}) {
-  return (
-    <div>AppContextProvider</div>
-  )
+  return <div>AppContextProvider</div>;
 }
 
-export default AppContextProvider
+export default AppContextProvider;
