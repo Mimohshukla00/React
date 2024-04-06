c514caf8b2ffbd67d1024cf050a47c1ef23e2eaa
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-06 23:02:18 +0530

    bloging

--- blogwebsite/src/Context/AppContextProvider.jsx
+++ blogwebsite/src/Context/AppContextProvider.jsx
@@ -6,6 +6,44 @@ function AppContextProvider({ children }) {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(null);
 
+
+
+  // data
+  const baseUrl = "https://codehelp-apis.vercel.app/api/get-blogs";
+
+  async  function fetchData() {
+    setLoading(true)
+    try {
+      const result = await fetch(baseUrl)
+      const data=await result.json();
+      console.log(data)
+    }
+    catch(error){
+      alert("Error Fetching Data")
+    }finally{
+      setLoading(false)
+
+
+    }
+  }
+      
+
+
+
+
+
+
+
+  const value={
+    posts,
+    loading,
+    page,
+    totalPages,
+    setLoading,setPage,
+    setPosts,
+    setTotalPages
+  }
+
   return <div>AppContextProvider</div>;
 }
 
